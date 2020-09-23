4ce1b3f38528c68604122cc19519db98a2b44a21
akash-12398 <napinderbajwa@gmail.com>
2020-09-23 15:10:03 -0500

    first submission

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace Method
+namespace Methods
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //------------Excerise 1-----------
+
+            // Name: Michael
+            // Favorite Color: Blue
+            // Favorite Animal: Walrus
+            // Favorite Band: The Beatles
+
+
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"My favorite color is {color}");
+
+            Console.WriteLine($"What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"My favorite animal is {animal}");
+
+
+            Console.WriteLine($"What band you like?");
+            var band = Console.ReadLine();
+
+
+            Console.WriteLine($"My favorite band is {band}");
         }
     }
 }
